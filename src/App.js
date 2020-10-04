44dce90b1c4869d769997b707e731996f984b9cc
YoussefHammi <youssef.hammi.oa@gmail.com>
2020-10-04 15:26:11 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1>Youssef Hammi</h1>
+        <br />
+        <img src="./keyborad.png" alt="myimage" />
+        <br />
+        <img src="./stone.png" alt="myimage" />
+        <br />
+      </div>
+      <video controls>
+        <source src="./farcry6.mp4" type="video/mp4" />
+      </video>
+    </>
   );
-}
-
+};
 export default App;
